389cad468e88d2503ef262a009b1c83cd4e0d862
Hano Varghese <hanojoby@gmail.com>
2025-07-11 22:48:02 +0530

    js ex 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0 ){
+        return 'ERROR';
+    }
+    let result = '';
+    for(let i=0;i<num;i++){
+        result = result + str;
+    }
 
+    return result;
 };
+
+let str = repeatString('hey', 3);
+console.log(str);
 
 // Do not edit below this line
 module.exports = repeatString;
