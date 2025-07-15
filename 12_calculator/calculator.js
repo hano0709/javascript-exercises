be234885e9cf4dda1974565bc647f9d628d8869e
Hano Varghese <hanojoby@gmail.com>
2025-07-15 23:49:21 +0530

    js exercise 12, 13

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length == 0) return 0;
+	return array.reduce((total, element) => {
+    return total+element;
+  })
 };
 
-const multiply = function() {
-
+const multiply = function(items) {
+  return items.reduce((total, element) => {
+    return total * element;
+  })
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let answer = 1;
+  for(let i=a;i>=1;i--){
+    answer = answer * i;
+  }
+
+  return answer;
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,36 @@
-const palindromes = function () {
+function cleanString(string){
+    string = string.toLowerCase();
+    const chars = string.split('');
+    console.log(chars);
+    for(let i=chars.length-1;i>=0;i--){
+        if((chars[i] >= 'a' && chars[i] <= 'z') || (chars[i] >= '0' && chars[i] <= '9')){}
+        else {
+            chars.splice(i, 1);
+        }
+    }
+    string = chars.join('')
+    return string;
+}
 
+const palindromes = function (string) {
+    string = cleanString(string);
+    let j = string.length-1;
+    let n = Math.floor(j/2);
+    let flag = true;
+    console.log(string);
+    console.log(j);
+    console.log(n);
+    for(let i=0;i<=n;i++){
+        if(string[i] != string[j]){
+            flag = false;
+            break;
+        }
+        j--;
+    }
+    return flag;
 };
+
+console.log(palindromes('r3ace3car'));
 
 // Do not edit below this line
 module.exports = palindromes;
