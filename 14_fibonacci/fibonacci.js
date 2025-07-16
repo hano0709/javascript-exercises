a27eff4de73eabd4b99ae9eea5cd7b34dbbbcd32
Hano Varghese <hanojoby@gmail.com>
2025-07-16 19:19:14 +0530

    js exercise 14, 15, 16

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if(num < 0) return 'OOPS';
+    if(num == 0) return 0;
+    if(num == 1 || num == 2) return 1;
+    let prev = 1;
+    let curr = 1;
+    for(let i=3;i<=num;i++){
+        let temp = curr + prev;
+        prev = curr;
+        curr = temp;
+    }
 
+    return curr;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const curr = new Date().getFullYear();
+    let value = arr.reduce((oldest, person) => {
+        let oldestAge;
+        if(oldest.yearOfDeath){
+            oldestAge = oldest.yearOfDeath  - oldest.yearOfBirth;
+        } else {
+            oldestAge = curr - oldest.yearOfBirth;
+        }
+        let personAge;
+        if(person.yearOfDeath){
+            personAge  = person.yearOfDeath  - person.yearOfBirth;
+        } else {
+            personAge = curr - person.yearOfBirth;
+        }
 
+        return personAge > oldestAge ? person : oldest;
+    }, arr[0]);
+
+    return value;
 };
 
 // Do not edit below this line
